2021e43bc17e1d23d960eb7cbc849d6f4f3e3fbf
Jasper <j.vanes1@student.avans.nl>
2022-10-13 15:48:02 +0200

    [EDIT] changed the layout of the Home page
    Added a border to the posts and the scroll-container, also centered everything

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -8,10 +8,10 @@ import YouTubeIcon from '@material-ui/icons/YouTube';
 function Footer() {
     return (
         <BottomNavigation>
-            <BottomNavigationAction label="Facebook" value="recents" icon={<FacebookIcon style={{ fill: "#3b5998", marginTop:"5rem" }} />} />
-            <BottomNavigationAction label="Twitter" value="favorites" icon={<TwitterIcon style={{ fill: "#1DA1F2", marginTop:"5rem" }} />} />
-            <BottomNavigationAction label="Instagram" value="nearby" icon={<InstagramIcon style={{ fill: " #C13584", marginTop:"5rem" }} />} />
-            <BottomNavigationAction label="YouTube" value="folder" icon={<YouTubeIcon style={{ fill: "#c4302b", marginTop:"5rem" }} />} />
+            <BottomNavigationAction label="Facebook" value="recents" icon={<FacebookIcon style={{ fill: "#3b5998"}} />} />
+            <BottomNavigationAction label="Twitter" value="favorites" icon={<TwitterIcon style={{ fill: "#1DA1F2"}} />} />
+            <BottomNavigationAction label="Instagram" value="nearby" icon={<InstagramIcon style={{ fill: " #C13584"}} />} />
+            <BottomNavigationAction label="YouTube" value="folder" icon={<YouTubeIcon style={{ fill: "#c4302b" }} />} />
         </BottomNavigation>
     )
 }
